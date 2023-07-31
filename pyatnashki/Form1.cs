335cb91470a631680fcc78453d64719302beb2a5
MaxKorobkov1997 <max.corobckov@yandex.ru>
2023-07-31 17:19:24 +0300

    pyatnashki
    pyatnashki

---
+++ pyatnashki/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pyatnashki
+{
+    public partial class Form1 : Form
+    {
+        int sise = 50;
+        int kol1 = 3;
+        const int kol = 3;
+        Button but;
+        Button[,] but3 = new Button[kol,kol];
+        private int[,] mao = new int[kol, kol];
+        public Form1()
+        {
+            InitializeComponent();
+            MapNamber();
+            CreatMap();
+            activ();
+            this.Width = kol1*sise+(kol1-1) * kol1;
+            this.Height = kol1 * sise + (kol1+1)  *3 +30;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreatMap()
+        {
+            for(int i = 0; i < kol; i++)
+            {
+                for (int j = 0; j < kol; j++)
+                {
+                    but3[i, j] = new Button();
+                    but = new Button();
+                    but.Size = new Size(sise, sise);
+                    but.Location = new Point(j * sise,i * sise);
+                    but.Click += MyNewButton_Click;
+                    but.BackColor = Color.White;
+                    Controls.Add(but);
+                    if (mao[i,j] != 0)
+                        but.Text = Convert.ToString(mao[i,j]);
+                    but3[i, j] = but;
+                    but3[i, j].Font = new Font("Times New Roman", 14);
+                }
+            }
+        }
+        private void MapNamber()
+        {
+            int a = 1;
+            for (int i = 0; i < kol; i++)
+            {
+                for (int j = 0; j < kol; j++)
+                {
+                    if (a != kol1*kol1)
+                    {
+                        mao[i, j] = a;
+                        a++;
+                    }
+                    else
+                        mao[i, j] = 0;
+                }
+            }
+        }
+        private void MyNewButton_Click(object sender, EventArgs e)
+        {
+
+            Button obbaut = sender as Button;
+            int a = mao[obbaut.Location.Y / sise, obbaut.Location.X / sise];
+            Prow(a);
+            activ();
+        }
+        private void Prow(int a)
+        {
+            bool q;
+            for (int i = 0; i < kol; i++)
+            {
+                for (int j = 0; j < kol; j++)
+                {
+                    q = true;
+                    if (mao[i, j] == a)
+                    {
+                        mao[i, j] = 0;
+                        but3[i, j].Text = " ";
+                        q = false;
+                    }
+                    if (mao[i, j] == 0 && q==true)
+                    {
+                        mao[i, j] = a;
+                        but3[i, j].Text = Convert.ToString(a);
+                    }
+                }
+            }
+        }
+        private void activ()
+        {
+            int x=0, y=0;
+            for (int i = 0; i < kol; i++)
+                for (int j = 0; j < kol; j++)
+                    but3[i, j].Enabled = false;
+            for (int i = 0; i < kol; i++)
+            {
+                for (int j = 0; j < kol; j++)
+                {
+                    if (mao[i, j] == 0)
+                    {
+                        x = i;
+                        y = j;
+                        if (x++ < kol1 - 1)
+                            but3[x, y].Enabled = true;
+                        x = i;
+                        if (y++ < kol1 - 1)
+                            but3[x, y].Enabled = true;
+                        y = j;
+                        if (y-- > 0)
+                            but3[x, y].Enabled = true;
+                        y = j;
+                        if (x-- > 0)
+                            but3[x, y].Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ pyatnashki/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("pyatnashki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pyatnashki")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("80e7c43a-5e11-454f-b000-5ff614498855")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
